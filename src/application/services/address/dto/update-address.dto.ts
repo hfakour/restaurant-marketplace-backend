56785d53ab5b6f026c01a14409c6af39f3d9d3bc
Hassan Fakour <91385916+hfakour@users.noreply.github.com>
2025-06-03 07:20:18 +0330

    start clean

--- src/application/services/address/dto/update-address.dto.ts
+++
@@ -1,7 +0,0 @@
-// src/application/dto/update-address.dto.ts
-
-import { InputType, PartialType } from '@nestjs/graphql';
-import { CreateAddressDto } from './create-address.dto';
-
-@InputType()
-export class UpdateAddressDto extends PartialType(CreateAddressDto) {}
